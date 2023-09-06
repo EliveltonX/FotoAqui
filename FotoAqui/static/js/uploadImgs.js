6d09cbc75526ef1d5e5c73c39ca2b801a76f5cd0
EliveltonX <Eliveltonalmeidapardini@gmail.com>
2023-09-05 23:01:25 -0400

    voltando ao desenvolvimento local

---
+++ FotoAqui/static/js/uploadImgs.js
@@ -0,0 +1,31 @@
+var input_email = document.getElementById("id_client_email");
+var file_input = document.getElementById("imgs");
+var image_container = document.getElementById("upload_imgs_preview");
+var numOfFiles = document.getElementById("numOfFiles");
+var submitBtn = document.getElementById("uploadSubmitBtn");
+
+file_input.setAttribute("onchange","preview()");
+input_email.disabled = true;
+submitBtn.disabled = true;
+
+
+function preview() {
+    image_container.innerHTML = "";
+    numOfFiles.textContent = file_input.files.length + ' Imagens Selecionadas.';
+    
+    for(i of file_input.files){
+        let reader = new FileReader();
+        let figure = document.createElement("figure");
+        let figCap = document.createElement("figcaption");
+        figCap.innerText = i.name;
+        figure.appendChild(figCap);
+        reader.onload=()=>{
+            let img = document.createElement("img");
+            img.setAttribute("src",reader.result);
+            img.setAttribute("class",'img_preview');
+            figure.insertBefore(img,figCap);
+        }
+        image_container.appendChild(figure);
+        reader.readAsDataURL(i);
+    }
+}
--- FotoAquiProject/settings.py
+++ FotoAquiProject/settings.py
@@ -77,17 +77,14 @@ WSGI_APPLICATION = 'FotoAquiProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        #SQLITE
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        
-        #POSTGREE:
         #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'basededados',
-        #'USER': os.environ.get('DATABASE_USER'),
-        #'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        #'HOST': os.environ.get('DATABASE_HOST'),
-        #'PORT': os.environ.get('DATABASE_PORT'),
+        #'NAME': "basededados",
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
